5208599bea7aabd0691e1897182ef7ac00a3d4ad
briangraj <brian.graj@gmail.com>
2017-04-08 22:34:59 -0300

    quitado los setter de alumno

--- src/main/java/pruebas/Alumno.java
+++ src/main/java/pruebas/Alumno.java
@@ -17,32 +17,15 @@ public class Alumno {
 		return code;
 	}
 
-	/*public void setCode(long code) {
-		this.code = code;
-	}*/
-
 	public String getFirst_name() {
 		return first_name;
 	}
-
-	/*public void setFirst_name(String first_name) {
-		this.first_name = first_name;
-	}*/
 
 	public String getLast_name() {
 		return last_name;
 	}
 
-	/*public void setLast_name(String last_name) {
-		this.last_name = last_name;
-	}*/
-
 	public String getGithub_user() {
 		return github_user;
 	}
-
-	/*public void setGithub_user(String github_user) {
-		this.github_user = github_user;
-	}*/
-	
 }
